0bf2fd61fa1aa223a8da3b17c9ba40d41b2c1156
tarendt <tarendt>
2012-09-21 16:13:59 +0000

    class ExtensionPointTags added

---
+++ org.eclipse.emf.refactor.metrics/src/org/eclipse/emf/refactor/metrics/ExtensionPointTags.java
@@ -0,0 +1,16 @@
+package org.eclipse.emf.refactor.metrics;
+
+public class ExtensionPointTags {
+
+	public static final String EXTENSION_POINT_TAG= "extension";
+	public static final String EXTENSION_POINT_NAME= "org.eclipse.emf.refactor.metrics";
+	public static final String METRIC_TAG = "metric";
+	public static final String METRIC_NAME_TAG = "metric_name";
+	public static final String METRIC_ID_TAG = "id";
+	public static final String METRIC_DESCRIPTION_TAG = "metric_description";
+	public static final String METRIC_METAMODEL_TAG = "metric_metamodel";
+	public static final String METRIC_CONTEXT_TAG = "metric_context";
+	public static final String METRIC_VALUE_TYPE_TAG = "metric_value_type";
+	public static final String METRIC_CALCULATE_CLASS_TAG = "metric_calculate_class";
+		
+}
