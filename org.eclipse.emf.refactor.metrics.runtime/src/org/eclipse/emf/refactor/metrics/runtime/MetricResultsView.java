04874769d80da052c711edfafeff41b7708fef9f
tarendt <tarendt>
2012-09-25 19:43:41 +0000

    extension point serving 'org.eclipse.ui.views' + class MetricResultsView + icon added

---
+++ org.eclipse.emf.refactor.metrics.runtime/src/org/eclipse/emf/refactor/metrics/runtime/MetricResultsView.java
@@ -0,0 +1,24 @@
+package org.eclipse.emf.refactor.metrics.runtime;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class MetricResultsView extends ViewPart {
+
+	public MetricResultsView() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
