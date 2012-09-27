875ecc8306a4267523291c64cd1a586b32327bff
tarendt <tarendt>
2012-09-27 09:38:23 +0000

    extension point NewWizards served + class NewMetricWizard added + icon added

---
+++ org.eclipse.emf.refactor.metrics.generator/src/org/eclipse/emf/refactor/metrics/generator/NewMetricWizard.java
@@ -0,0 +1,26 @@
+package org.eclipse.emf.refactor.metrics.generator;
+
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
+
+public class NewMetricWizard extends Wizard implements INewWizard {
+
+	public NewMetricWizard() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean performFinish() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
