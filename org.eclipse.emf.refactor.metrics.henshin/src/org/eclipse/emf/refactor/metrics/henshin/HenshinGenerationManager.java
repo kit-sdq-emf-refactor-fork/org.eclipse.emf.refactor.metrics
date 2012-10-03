54486fe72b3b0ac5793f0084596ab9238420acb1
tarendt <tarendt>
2012-10-03 07:40:01 +0000

    class HenshinGenerationManager added

---
+++ org.eclipse.emf.refactor.metrics.henshin/src/org/eclipse/emf/refactor/metrics/henshin/HenshinGenerationManager.java
@@ -0,0 +1,75 @@
+package org.eclipse.emf.refactor.metrics.henshin;
+
+import java.util.List;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.emf.codegen.jet.JETException;
+import org.eclipse.emf.refactor.metrics.generator.managers.DependenciesManager;
+import org.eclipse.emf.refactor.metrics.generator.managers.GenerationManager;
+import org.eclipse.emf.refactor.metrics.generator.managers.XMLPluginFileManager;
+import org.eclipse.jdt.core.IClasspathEntry;
+import org.eclipse.jdt.core.JavaCore;
+import org.osgi.framework.Bundle;
+
+public class HenshinGenerationManager extends GenerationManager {
+	
+	private static final String HENSHIN_TEMPLATE_CLASS_NAME = "HenshinCalculateClassTemplate";
+	
+	private HenshinGenerationManager() {
+		templateDirectory = setTemplateDirectory();
+		classpathEntries = setClassPathEntries();
+		System.out.println("HenshinGenerationManager initialized!");
+	}
+	
+	public static HenshinGenerationManager getInstance() {
+		if (instance == null) {
+			instance = new HenshinGenerationManager();
+		}
+		return (HenshinGenerationManager) instance;
+	}
+	
+	protected List<IClasspathEntry> setClassPathEntries() {
+		List<IClasspathEntry> cpe = super.setClassPathEntries();
+		Bundle bundle = Platform.getBundle(Activator.PLUGIN_ID);
+	    // add org.eclipse.emf.refactor.metrics.henshin to class path
+	    String version = (String) bundle.getHeaders().get(BUNDLEVERSION);
+	    cpe.add(JavaCore.newLibraryEntry(new Path(PLUGINSPATH + 
+	    		Activator.PLUGIN_ID + "_" + version + ".jar"), null, null));
+	    return cpe;
+	}
+	
+	public static void createNewMetric(IProgressMonitor monitor,
+			HenshinMetricInfo metricInfo, IProject targetProject) {
+		System.out.println(metricInfo);
+		DependenciesManager.updateDependencies(metricInfo);
+		createCalculateClass(monitor, metricInfo);		
+		XMLPluginFileManager.createMetricEntry(metricInfo.getProjectPath(), 
+				metricInfo.getName(), metricInfo.getId(), 
+				metricInfo.getDescription(), metricInfo.getMetamodel(), 
+				metricInfo.getContext(), metricInfo.getPackage()+"."+metricInfo.getClassName());
+		try {
+			targetProject.refreshLocal(IProject.DEPTH_INFINITE, monitor);
+			targetProject.refreshLocal(IProject.DEPTH_INFINITE, monitor);
+		} catch (CoreException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static void createCalculateClass(IProgressMonitor monitor, HenshinMetricInfo metricInfo) {
+		String generatedCode = "";
+		String templateName = HENSHIN_TEMPLATE_CLASS_NAME;
+		try {
+			generatedCode = generateCode(monitor, templateName, metricInfo);
+		    saveCode(monitor, generatedCode, metricInfo);
+		} catch (JETException e) {
+			e.printStackTrace();
+		} catch (CoreException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
