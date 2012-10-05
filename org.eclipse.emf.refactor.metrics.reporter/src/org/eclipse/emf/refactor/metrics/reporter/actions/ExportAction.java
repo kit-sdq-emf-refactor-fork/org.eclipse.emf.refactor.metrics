73713b6fe6802bd743860507557070b3385fa56e
tarendt <tarendt>
2012-10-05 09:50:22 +0000

    ExportAction implemented

--- org.eclipse.emf.refactor.metrics.reporter/src/org/eclipse/emf/refactor/metrics/reporter/actions/ExportAction.java
+++ org.eclipse.emf.refactor.metrics.reporter/src/org/eclipse/emf/refactor/metrics/reporter/actions/ExportAction.java
@@ -1,6 +1,11 @@
 package org.eclipse.emf.refactor.metrics.reporter.actions;
 
+import org.eclipse.emf.refactor.metrics.reporter.managers.ReportManager;
+import org.eclipse.emf.refactor.metrics.reporter.ui.ReportingWizard;
+import org.eclipse.emf.refactor.metrics.reporter.ui.ReportingWizardDialog;
+import org.eclipse.emf.refactor.metrics.runtime.managers.RuntimeManager;
 import org.eclipse.jface.action.Action;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.ISharedImages;
 import org.eclipse.ui.PlatformUI;
 
@@ -8,13 +13,19 @@ public class ExportAction extends Action {
 
 	public ExportAction() {
 		super();
-		this.setText("to do");
-		this.setToolTipText("to do");
+		this.setText("Export Results");
+		this.setToolTipText("Export the results list in various reports");
 		this.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().getImageDescriptor(ISharedImages.IMG_ETOOL_PRINT_EDIT));
 	}
 
 	public void run() {
-		System.out.println("IN: ExportAction:: run() => TO DO ...");
+		RuntimeManager.getInstance();
+		Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();;
+		ReportManager reportManager = new ReportManager(shell, RuntimeManager.getResultsViewInput());	
+		ReportingWizard wizard = new ReportingWizard(reportManager);
+		ReportingWizardDialog dialog = new ReportingWizardDialog(shell, wizard);
+		dialog.create();
+		dialog.open();
 	}
 
 }
